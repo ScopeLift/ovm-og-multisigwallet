f872cd2fb59c869a2ac781723fc4292f4d6b9236
Ben DiFrancesco <ben@scopelift.co>
2021-02-17 10:30:57 -0500

    WIP: still broken ovm

--- truffle-config.js
+++ truffle-config.js
@@ -4,13 +4,13 @@ console.log('GANACHE', ganache);
 
 module.exports = {
   networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gas: 4000000,
-      gasPrice: 10000000000, // 10 gwei
-    }
+    // development: {
+    //   host: "localhost",
+    //   port: 8545,
+    //   network_id: "*", // Match any network id
+    //   gas: 4000000,
+    //   gasPrice: 10000000000, // 10 gwei
+    // }
   },
   compilers: {
     solc: {
