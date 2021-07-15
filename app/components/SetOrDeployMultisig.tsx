a211b92d5b73c0cdf22f7b67131b1ab35348f107
wildmolasses <changes@gmail.com>
2021-07-15 15:32:52 -0500

    improve initial form

--- app/components/SetOrDeployMultisig.tsx
+++ app/components/SetOrDeployMultisig.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/router';
 import { Contract } from '@ethersproject/contracts';
 import { abi as factoryAbi } from 'abi/MultiSigWalletFactory.json';
 import { config } from 'config';
+import { ArrowCircleRightIcon } from '@heroicons/react/outline';
+import { LightningBoltIcon } from '@heroicons/react/solid';
 
 export const SetOrDeployMultisig = ({ address }: { address?: string }) => {
   const { library, chainId } = useWeb3React<Web3Provider>();
@@ -40,88 +42,107 @@ export const SetOrDeployMultisig = ({ address }: { address?: string }) => {
 
   if (address) return <></>;
   return (
-    <div className="flex h-full flex-col items-center mt-10">
-      <div className="py-2 px-3 rounded-t bg-gradient-to-r w-1/2 from-purple-400 to-red-500 font-semibold text-white">
-        <h2>OG Gnosis Multisig on OVM</h2>
-      </div>
-      <div className="bg-gray-200 w-1/2 p-5">
-        {error && <div>{error}</div>}
-        <div>
-          <form>
-            <label>Enter multisig contract address:</label>
-            <input
-              className="block m-2 p-2 w-5/6 font-mono mx-auto"
-              onChange={(e) => setInputAddress(e.target.value)}
-            />
-            <button
-              className="block bg-gradient-to-r from-green-400 to-blue-500 px-3 py-2 text-white text-sm font-semibold rounded mx-auto"
-              onClick={openMultisig}
-            >
-              Open Multisig
-            </button>
-          </form>
+    <div className="flex h-full flex-col items-center my-10">
+      <div className="w-1/2 max-w-xl">
+        <div className="px-7 py-5 rounded-t-lg bg-gradient-to-r from-purple-400 to-red-400 font-semibold text-white">
+          <h2 className="text-lg leading-6 font-medium text-gray-100">OG Gnosis Multisig on OVM</h2>
         </div>
-        <div className="mx-auto my-5 text-center">- or -</div>
-        <div>
-          Create multisig:
-          <form className="">
-            <ul className="block mx-auto">
-              {[...Array(nOwnerInputs).keys()].map((n) => (
-                <li key={n} className="my-1 p-2 w-5/6 mx-auto">
-                  <label className="text-sm text-gray-600">Owner {n + 1}:</label>
-                  <input
-                    className="font-mono w-full my-1 mx-2 p-1"
-                    onChange={(e) => {
-                      const ownerArr = owners;
-                      ownerArr[n] = e.target.value;
-                      return setOwners(ownerArr);
-                    }}
-                  />
-                </li>
-              ))}
-              <li className="text-center">
-                {nOwnerInputs > 1 && (
+        <div className="bg-gradient-to-b from-gray-100 to-gray-200 py-5 px-8 rounded-b-lg">
+          {error && <div>{error}</div>}
+          <div className="my-5">
+            <form className="my-2">
+              <label className="block text-sm font-medium text-gray-700">
+                Enter multisig contract address:
+              </label>
+              <input
+                type="text"
+                className="block my-2 font-mono text-sm w-full rounded-md border-gray-300"
+                onChange={(e) => setInputAddress(e.target.value)}
+              />
+              <button
+                className="group flex justify-center items-center px-4 py-2 mt-4 mb-6 mx-auto font-medium text-white text-sm bg-gradient-to-r from-green-400 to-blue-500 opacity-80 hover:opacity-90 rounded"
+                onClick={openMultisig}
+              >
+                View Multisig <ArrowCircleRightIcon className="ml-1 -mr-1 h-4 w-4" />
+              </button>
+            </form>
+          </div>
+          <Divider />
+          <div className="my-5">
+            <h3 className="block text-sm font-medium text-gray-700">Deploy new multisig:</h3>
+            <form>
+              <ul className="block mx-auto">
+                {[...Array(nOwnerInputs).keys()].map((n) => (
+                  <li key={n} className="my-1 p-2">
+                    <label className="text-sm text-gray-700">Owner {n + 1}:</label>
+                    <input
+                      type="text"
+                      className="block my-1 font-mono w-full rounded-md border-gray-300 text-sm"
+                      onChange={(e) => {
+                        const ownerArr = owners;
+                        ownerArr[n] = e.target.value;
+                        return setOwners(ownerArr);
+                      }}
+                    />
+                  </li>
+                ))}
+                <li className="text-center">
+                  {nOwnerInputs > 1 && (
+                    <button
+                      className=" text-red-500 border border-red-300 px-1 text-sm rounded mr-2"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setOwners(owners.slice(0, nOwnerInputs - 1));
+                        setNOwnerInputs(nOwnerInputs - 1);
+                      }}
+                    >
+                      - Remove Owner
+                    </button>
+                  )}
                   <button
-                    className="bg-gray-200 text-red-500 border border-red-300 px-1 text-sm rounded mr-2"
+                    className=" text-gray-500 border border-gray-400 px-1 text-sm rounded"
                     onClick={(e) => {
                       e.preventDefault();
-                      setOwners(owners.slice(0, nOwnerInputs - 1));
-                      setNOwnerInputs(nOwnerInputs - 1);
+                      setNOwnerInputs(nOwnerInputs + 1);
                     }}
                   >
-                    - Remove Owner
+                    + Add Additional Owner
                   </button>
-                )}
-                <button
-                  className="bg-gray-200 text-gray-500 border border-gray-300 px-1 text-sm rounded"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setNOwnerInputs(nOwnerInputs + 1);
-                  }}
-                >
-                  + Add Additional Owner
-                </button>
-              </li>
-              <li className="m-2 p-2 w-5/6 mx-auto">
-                <label className="text-sm text-gray-500 p-1">
-                  # signatures required per transaction:
-                </label>
-                <input
-                  className="block p-1 m-2"
-                  type="number"
-                  value={nConfirmations}
-                  onChange={(e) => setNConfirmations(parseInt(e.target.value))}
-                />
-              </li>
-            </ul>
-            <button
-              className="block bg-gradient-to-r from-green-400 to-blue-500 px-3 py-2 text-white text-sm font-semibold rounded mx-auto"
-              onClick={deployMultisig}
-            >
-              Deploy Multisig
-            </button>
-          </form>
+                </li>
+                <li className="mx-2 my-6 w-5/6">
+                  <label className="text-sm text-gray-700">
+                    # signatures required per transaction:
+                  </label>
+                  <input
+                    className="block my-1 text-sm rounded-md border-gray-300"
+                    type="number"
+                    value={nConfirmations}
+                    onChange={(e) => setNConfirmations(parseInt(e.target.value))}
+                  />
+                </li>
+              </ul>
+              <button
+                className="group flex justify-center items-center px-4 py-2 mt-4 mb-6 mx-auto font-medium text-white text-sm bg-gradient-to-r from-green-400 to-blue-500 opacity-80 hover:opacity-90 rounded"
+                onClick={deployMultisig}
+              >
+                Deploy Multisig <LightningBoltIcon className="ml-2 -mr-1 h-4 w-4" />
+              </button>
+            </form>
+          </div>
         </div>
+      </div>
+    </div>
+  );
+};
+
+const Divider = () => {
+  return (
+    <div className="relative">
+      <div className="absolute inset-0 flex items-center" aria-hidden="true">
+        <div className="w-full border-t border-gray-300" />
+      </div>
+      <div className="relative flex justify-center">
+        <span className="px-2 bg-gray-100 text-sm text-gray-500">or</span>
       </div>
     </div>
   );
