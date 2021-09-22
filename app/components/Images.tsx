a2f77aecb7940a915fc6e564724562b1a6b68b6a
radchukd <radchukd@protonmail.com>
2021-09-22 18:53:11 +0200

    Add title Add cursors to tailwind config Add tx data modal Fix tx table paging Fix CloseIcon props

--- app/components/Images.tsx
+++ app/components/Images.tsx
@@ -1,16 +1,24 @@
-export const CloseIcon = ({ className, onClick }) => {
+import { FC, MouseEvent } from 'react';
+
+interface IconProps {
+  className?: string;
+  size?: number;
+  onClick?: (e: MouseEvent) => void;
+}
+
+export const CloseIcon: FC<IconProps> = ({ className, size = 20, onClick }) => {
   return (
     <div className={className} onClick={onClick}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
+        width={size}
+        height={size}
+        viewBox={`0 0 ${size} ${size}`}
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <line x1="18" y1="6" x2="6" y2="18"></line>
         <line x1="6" y1="6" x2="18" y2="18"></line>
